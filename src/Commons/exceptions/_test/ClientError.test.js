d4b560b87adcc4ed3e2b32d26d551467510b6105
Diki Rahman <dikirahman.me@gmail.com>
2021-10-04 21:02:54 +0700

    init

---
+++ src/Commons/exceptions/_test/ClientError.test.js
@@ -0,0 +1,8 @@
+const ClientError = require('../ClientError');
+
+describe('ClientError', () => {
+  it('should throw error when directly use it', () => {
+    expect(() => new ClientError('')).
+        toThrowError('cannot instantiate abstract class');
+  });
+});
